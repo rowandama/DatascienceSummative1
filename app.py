129f53cd6accac81155d0fc542648727b327b0d7
Rowan Damaschin <rowan.damaschin@lis.ac.uk>
2025-11-14 17:20:37 +0000

    Summative1 page finished just need to do shiny additionals

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from shiny import App, render, ui
+import pandas as pd
 
 app_ui = ui.page_fluid(
     ui.output_plot("attendance_plot"),  
@@ -8,11 +9,11 @@ app_ui = ui.page_fluid(
 def server(input, output, session):
     @render.plot
     def attendance_plot():  
-        df = pd.DataFrame("attendance_anonymised.csv")
+        df = pd.read_csv("DatascienceSummative1/attendance_anonymised-1.csv") 
         df = df.rename(columns={'Unit Instance Code': 'Module Code', 'Calocc Code': 'Year', 'Long Description': 'Module Name', 'Register Event ID': 'Event ID', 'Register Event Slot ID': 'Event Slot ID', 'Planned Start Date': 'Date', 'is Positive': 'Has Attended', 'Postive Marks': 'Attended', 'Negative Marks': 'Not Atteneded', 'Usage Code': 'Attendance Code'})
         df['Date']= pd.to_datetime(df['Date'])
         history_df = df[df['Module Name'] == 'History']
-        history_df.groupby(history_df['Date'].dt.date)['Attended'].mean().plot(kind="line", grid=True, title="Average Attendance for History Module Over Time", ylabel="Average Attendance", xlabel="Date", figsize=(10,6), marker='o')
+        fig = history_df.groupby(history_df['Date'].dt.date)['Attended'].mean().plot(kind="line", grid=True, title="Average Attendance for History Module Over Time", ylabel="Average Attendance", xlabel="Date", figsize=(10,6), marker='o')
         return fig
     
 
